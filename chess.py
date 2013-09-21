481c517bc906bac295b0a457df4dd646003954f6
Kevin Lacker <lacker@gmail.com>
2013-09-21 10:54:03 -0700

    color-of-squares quiz

---
+++ chess.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+import random
+import time
+
+"""
+The square like (2, 2) from the name like c3.
+"""
+def square_from_name(name):
+  if len(name) != 2:
+    raise ValueError, ("%s should be 2 characters long" % name)
+  x = ord(name[0]) - ord("a")
+  if x < 0 or x > 7:
+    raise ValueError, "first char invalid: %s" % name[0]
+  y = ord(name[1]) - ord("1")
+  if y < 0 or y > 7:
+    raise ValueError, "second char invalid: %s" % name[1]
+  return (x, y)
+
+"""
+The name like g6 from the square like (6, 7).
+"""
+def name_from_square(square):
+  x, y = square
+  return chr(ord("a") + x) + chr(ord("1") + y)
+
+"""
+Returns a color string, either 'white' or 'black', for the square.
+"""
+def color(square):
+  x, y = square
+  if (x + y) % 2:
+    return 'white'
+  return 'black'
+
+"""
+Returns a random square in a format like (4, 5).
+"""
+def random_square():
+  return (random.randint(0, 7), random.randint(0, 7))
+
+"""
+Runs a quiz given the particular one-round function.
+The function should return 0 or 1 for success.
+"""
+def quiz(one_round, num_rounds):
+  score = 0
+  start = time.time()
+  for _ in range(num_rounds):
+    score += one_round()
+  end = time.time()
+  duration = end - start
+  print "You scored %d / %d = %d%%." % (score, num_rounds,
+                                        int(100.0 * score / num_rounds))
+  print "You spent %.2fs per question." % (duration / num_rounds)
+  print
---
+++ color.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+"""
+A quiz to see if you know what the colors are for squares on the chessboard.
+
+Phase 1 of Rensch blindfold training.
+"""
+import chess
+import sys
+
+"""
+Does one round of the quiz. Returns your score, 0 or 1.
+"""
+def one_round():
+  square = chess.random_square()
+  name = chess.name_from_square(square)
+  color = chess.color(square)
+  print "What color is %s?" % name
+  answer = ""
+  while len(answer) == 0:
+    answer = sys.stdin.readline().strip().lower()
+  if answer[0] == color[0]:
+    print "Correct!"
+    print
+    return 1
+  print "No. %s is %s." % (name, color)
+  board = """
+- # - # - # - # 
+# - # - # - # - 
+- # - # - # - # 
+# - # - # - # - 
+- # - # - # - # 
+# - # - # - # - 
+- # - # - # - # 
+# - # - # - # -
+  """
+  board = board.replace("#", u"\u25a0")
+  board = board.replace("-", u"\u25a1")
+  print board
+  print "Hit enter to continue."
+  sys.stdin.readline()
+  print "\n" * 100
+  return 0
+
+"""
+Does the whole quiz.
+"""
+def main():
+  chess.quiz(one_round, 20)
+
+if __name__ == "__main__":
+  main()
