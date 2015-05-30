9c8b023ac9ab6bbb31b70e56258c751a0c731346
Kevin Lacker <lacker@gmail.com>
2015-05-30 15:20:56 -0700

    more helpers for chess

--- board.js
+++ board.js
@@ -1,19 +1,30 @@
 #!/usr/local/bin/node
 // Written for node v0.10.20
 // TODO: convert to ES6
+// TODO: use modules
+
+// The chess colors. Convenient for multiplying vectors.
+var WHITE = 1
+var EMPTY = 0
+var BLACK = -1
 
 function Board() {
   // Each square's content has a character representation.
   // Standard chess notation for pieces, caps is white, . is empty.
   // [0][0] is a1, [7][0] is h1, [7][7] is h8.
-  this.squares = ["RP....pr",
-                  "NP....pn",
-                  "BP....pb",
-                  "QP....pq",
-                  "KP....pk",
-                  "BP....pb",
-                  "NP....pn",
-                  "RP....pr"]
+  // Typically we refer to these two indices as x and y.
+  this.board = ["RP....pr",
+                "NP....pn",
+                "BP....pb",
+                "QP....pq",
+                "KP....pk",
+                "BP....pb",
+                "NP....pn",
+                "RP....pr"]
+
+  // The square that an en-passant capture can move into.
+  // null if there is none
+  this.passant = null
 }
 
 // "a1" -> [0, 0]
@@ -28,15 +39,48 @@ function squareForCoords(x, y) {
           String.fromCharCode("1".charCodeAt(0) + y))
 }
 
+function validCoords(x, y) {
+  return x >= 0 && x < 8 && y >= 0 && y < 8
+}
+
+Board.prototype.colorForCoords = function(x, y) {
+  var piece = this.board[x][y]
+  if (piece >= "a" && piece <= "z") {
+    return BLACK
+  }
+  if (piece >= "A" && piece <= "Z") {
+    return WHITE
+  }
+  if (piece == ".") {
+    return EMPTY
+  }
+  throw ("invalid piece: " + piece)
+}
+
 // square is in "e4" type notation
 Board.prototype.pieceForSquare = function(square) {
   var coords = coordsForSquare(square)
   var x = coords[0]
   var y = coords[1]
-  return this.squares[x][y]
+  return this.board[x][y]
 }
 
-// Tests
-var b = new Board()
-if (b.pieceForSquare("e1") != "K") throw "pieceForSquare failed"
+// A list of [x, y] coords that a knight can move to.
+Board.prototype.knightMoves = function(color, x, y) {
+  throw "TODO"
+}
 
+// Testing
+
+function testEq(name, foo, bar) {
+  if (foo != bar) {
+    throw ("in " + name + " test: " + foo + " != " + bar)
+  }
+}
+
+var b = new Board()
+testEq("pieceForSquare", "K", b.pieceForSquare("e1"))
+testEq("validCoords", false, validCoords(4, 8))
+testEq("colorForCoords", WHITE, b.colorForCoords(6, 1))
+testEq("colorForCoords", BLACK, b.colorForCoords(3, 7))
+testEq("colorForCoords", EMPTY, b.colorForCoords(2, 2))
