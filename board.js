379dd5c6ec01a5ef5df59780ca874592c522a823
Kevin Lacker <lacker@gmail.com>
2015-06-02 23:09:39 -0700

    validMoves test failin

--- board.js
+++ board.js
@@ -439,6 +439,11 @@ testEq("bishopMoves", 8, b.bishopMoves(3, 3).length)
 testEq("rookMoves", 11, b.rookMoves(3, 3).length)
 testEq("queenMoves", 19, b.queenMoves(3, 3).length)
 testEq("validMovesIgnoringCheck", 20, b.validMovesIgnoringCheck().length)
+testEq("movesIntoCheck", false, b.movesIntoCheck(0, 1, 0, 2))
+testEq("validMoves", 20, b.validMoves().length)
+
+console.log(JSON.stringify(b.validMovesIgnoringCheck()))
+console.log(JSON.stringify(b.validMoves()))
 
 testCheckmate("scholars mate", [
   "e2e4", "e7e5",
