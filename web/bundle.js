355f0e030965faae4d35ddb97cf195b71f51c0e5
Kevin Lacker <lacker@gmail.com>
2015-06-04 22:17:35 -0700

    es6 not working w webpack

--- web/bundle.js
+++ web/bundle.js
@@ -46,10 +46,10 @@
 
 	// import board from "../lib/board.js"
 
-	// let five = 5
-	// console.log("hello console world. five = " + five)
+	var five = 6
+	console.log("hello console world. five = " + five)
 
-	var body = document.querySelector("body");
+	var body = document.querySelector("body")
 	body.textContent = "hello chess world"
 
 
