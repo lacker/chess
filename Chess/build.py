5948df5c16e96be4b48faf1950cab2bb520e485b
Kevin Lacker <lacker@gmail.com>
2015-07-29 22:25:12 -0700

    some script improvement

--- Chess/build.py
+++ Chess/build.py
@@ -2,8 +2,16 @@
 
 import os
 
-images = os.path.join(*(os.path.split(__file__)[:-1] + ("images",)))
+base_dir = os.path.join(*os.path.split(__file__)[:-1])
+images = base_dir + "/images"
 
 for image in os.listdir(images):
+  # Strip off the .png
+  base = image.split(".")[0]
+
+  # The source picture
   fname = os.path.join(images, image)
   print fname
+
+  # Check if the destination dir exists
+  target_dir = os.path.join
