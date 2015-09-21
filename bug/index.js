782a3288aaaeb18e59324a0877de4e051e206200
Kevin Lacker <lacker@gmail.com>
2015-09-20 22:21:38 -0700

    well the code is syntactically correct

--- bug/index.js
+++ bug/index.js
@@ -1,8 +1,30 @@
+var bodyParser = require('body-parser');
 var express = require('express');
+var r = require('rethinkdb');
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function (request, response) {
+  r.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
+    if (err) {
+      throw err;
+    }
+    r.db('dev').tableCreate('Message').run(conn, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      console.log(result);
+      r.table('Message').insert({content: 'sup world'}).run(
+        conn,
+        function(err, result) {
+          if (err) {
+            throw err;
+          }
+          console.log(result);
+          response.send("Got to the center of the nest");
+        });
+    });
+  });
 });
 
 var server = app.listen(3000, function () {
