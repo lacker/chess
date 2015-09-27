3183edd746e68ccd7a0432b92657b464d2e1e434
Kevin Lacker <lacker@gmail.com>
2015-09-27 12:05:59 -0700

    add readlining

--- bug/es6/chat.js
+++ bug/es6/chat.js
@@ -1,4 +1,11 @@
 let r = require('rethinkdbdash')();
+let readline = require('readline');
+
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 
 // Display stuff from the chat
 r.table('Message').run({cursor: true}).then((cursor) => {
@@ -9,4 +16,7 @@ r.table('Message').run({cursor: true}).then((cursor) => {
   });
 });
 
-// Typing stuff in should create
+// Typing stuff in should create a new message
+rl.on('line', (content) => {
+  console.log('readlined content:', content);
+});
