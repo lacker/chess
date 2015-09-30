7f6ab6508189246ec3da7f51fe8ccc06e6149425
Kevin Lacker <lacker@gmail.com>
2015-09-29 21:02:59 -0700

    add timestamps. in PDT. yuck

--- bug/es6/chat.js
+++ bug/es6/chat.js
@@ -8,18 +8,19 @@ let rl = readline.createInterface({
 
 
 // Display stuff from the chat
-r.table('Message').run({cursor: true}).then((cursor) => {
+r.table('Message').orderBy('time').run({cursor: true}).then((cursor) => {
   cursor.each((err, message) => {
     if (message) {
-      console.log(message.content);
+      console.log(message.content, message.time);
     }
   });
 });
 
 // Typing stuff in should create a new message
 rl.on('line', (content) => {
+  let time = new Date();
   console.log('inserting ' + content);
-  r.table('Message').insert({content}).run().done();
+  r.table('Message').insert({content, time}).run().done();
 });
 
-// TODO: sort by time, make the display stuff listen for liveness
+// TODO: make live-listening work
