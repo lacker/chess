d63c60ccce59780d99cbf872f41390dd42993982
Kevin Lacker <lacker@gmail.com>
2015-10-06 21:43:29 -0700

    one basic unit test

--- bug/es6/tests/board.spec.js
+++ bug/es6/tests/board.spec.js
@@ -1,1 +1,9 @@
-console.log('in board.spec.js');
+var board = require('../board');
+var Board = board.Board;
+
+describe('basic board', () => {
+  it('basic moves', () => {
+    var b = new Board();
+    expect(b.pieceForSquare('e1')).toEqual('K');
+  });
+});
