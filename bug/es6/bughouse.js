c351ef28877e568184729e1d46a8ebf5f6c6d335
Kevin Lacker <lacker@gmail.com>
2015-10-07 16:20:24 -0700

    initial bughouse

---
+++ bug/es6/bughouse.js
@@ -0,0 +1,9 @@
+// A game of bughouse has two teams, 0 and 1.
+// It has four players, White and Black for each team.
+class Bughouse {
+  constructor() {
+    this.boards = [new Board(), new Board()];
+  }
+}
+
+// TODO: write a makeMove function
